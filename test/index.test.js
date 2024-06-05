a05abe285870ce539cfaef82c6bb6637d2e02678
RogelioLB <rogelio20052011@gmail.com>
2024-06-05 16:42:56 -0700

    NPM Publish

---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+import ffmMT from 'ffmetadata';
+
+import { Video, Audio } from '../dist/index';
+
+const link = 'https://www.youtube.com/watch?v=_qfnSJeSFLs';
+
+async function test(url) {
+  const data = await Audio({
+    url,
+    onDownloading: (d) => console.log(`Downloaded ${d.percentage}%`),
+    directory: './',
+  });
+  console.log(data.pathfile);
+  ffmMT.read(data.pathfile, (err, res) => {
+    if (err) console.error('Error reading metadata', err);
+    else console.log(res);
+  });
+
+  console.log(data.message);
+}
+
+test(link);
